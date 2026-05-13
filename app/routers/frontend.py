8fcb01268794441a87d8b3186f15ab1f4fd9d0e2
José Enrique Sánchez <josesanchezb7@gmail.com>
2026-05-13 23:29:51 +0100

    fix import schemas

--- app/routers/frontend.py
+++ app/routers/frontend.py
@@ -4,7 +4,7 @@ from fastapi.templating import Jinja2Templates
 from sqlalchemy.orm import Session
 
 from ..db import get_db
-from .. import models, Schemas
+from .. import models, schemas
 from app.routers.misas import listar_misas  # 🔥 SOLO esto, quitamos obtener_liturgia
 
 router = APIRouter(tags=["frontend"])
